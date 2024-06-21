47978f23556bd53bc1c8790a5d94b9efcfd092c8
Jozef S <franck.vukelic@gmail.com>
2024-06-21 17:01:05 +0700

    Add CustomButton component

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
 
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
 
-export default CustomButton
+      {isLoading && (
+        <ActivityIndicator
+          animating={isLoading}
+          color="#fff"
+          size="small"
+          className="ml-2"
+        />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
