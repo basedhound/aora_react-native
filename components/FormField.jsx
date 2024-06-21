13692ab11ef4db4b332a16766e79a5adae8cc95e
Jozef S <franck.vukelic@gmail.com>
2024-06-21 18:00:18 +0700

    Add FormField component + SignIn/Up pages

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+//
+import { icons } from "../constants";
 
-const FormField = () => {
+//!
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  // Constants
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <View>
-      <Text>FormField</Text>
-    </View>
-  )
-}
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-export default FormField
+      <View className="flex flex-row items-center w-full h-16 px-4 border-2 bg-black-100 rounded-2xl border-black-200 focus:border-secondary">
+        <TextInput
+          className="flex-1 text-base text-white font-psemibold"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
