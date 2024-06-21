ad49fbc363ee5eef62ee2ee49f7dfb34c66beaea
Jozef S <franck.vukelic@gmail.com>
2024-06-21 19:15:02 +0700

    Update typo

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -52,7 +52,7 @@ const SignUp = () => {
           />
 
           <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
-            Sign Up to Aora
+            Sign up to Aora
           </Text>
 
           <FormField
