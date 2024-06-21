c360eb8c3c819a1a3dbbb24b85bb1e1604a25495
Jozef S <franck.vukelic@gmail.com>
2024-06-21 17:17:06 +0700

    Add StatusBar to Home page

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { Image, ScrollView, Text, View } from "react-native";
-import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "../constants";
+import { Image, ScrollView, Text, View } from "react-native";
+import { Redirect, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+//
 import CustomButton from "../components/CustomButton";
+import { images } from "../constants";
 
+//!
 export default function App() {
   return (
     <SafeAreaView className="h-full bg-primary">
@@ -12,7 +14,7 @@ export default function App() {
         contentContainerStyle={{
           height: "100%",
         }}>
-        <View className="items-center justify-center w-full h-full px-4">
+        <View className="items-center justify-center w-full min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -36,22 +38,22 @@ export default function App() {
               source={images.path}
               className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
               resizeMode="contain"
-            />         
+            />
           </View>
 
           <Text className="text-sm text-center text-gray-100 font-pregular mt-7">
             Where Creativity Meets Innovation: Embark on a Journey of Limitless
             Exploration with Aora
           </Text>
-          
+
           <CustomButton
             title="Continue with Email"
             handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
-          
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
