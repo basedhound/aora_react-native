715ca58dc4180dcc2ababaddadf27e4b77852886
Jozef S <franck.vukelic@gmail.com>
2024-06-22 19:11:56 +0700

    Update username which was hardcoded

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 //
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 //
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
@@ -13,6 +14,7 @@ import VideoCard from "../../components/VideoCard";
 
 //!
 const Home = () => {
+  const { user, setUser, setIsLogged } = useGlobalContext();
   // Fetching Posts
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
@@ -36,10 +38,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Frank
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
