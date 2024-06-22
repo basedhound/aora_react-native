1a6925f53fa45390a9265faacee7f8d36da7f5ac
Jozef S <franck.vukelic@gmail.com>
2024-06-22 19:08:34 +0700

    Add InfoBox component

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,22 +1,24 @@
 import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Image, FlatList, TouchableOpacity } from "react-native";
-
+//
 import { icons } from "../../constants";
 import useAppwrite from "../../lib/useAppwrite";
 import { getUserPosts, signOut } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
 import { EmptyState, InfoBox, VideoCard } from "../../components";
 
+//!
 const Profile = () => {
+  // Instances
   const { user, setUser, setIsLogged } = useGlobalContext();
   const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
 
+  // Logout function
   const logout = async () => {
     await signOut();
     setUser(null);
     setIsLogged(false);
-
     router.replace("/sign-in");
   };
 
