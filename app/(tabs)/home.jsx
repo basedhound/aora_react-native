c1019b8e747a84cc21283cf059103aca82504117
Jozef S <franck.vukelic@gmail.com>
2024-06-22 18:49:24 +0700

    Add Search page, need to fix issues later

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { View, Text, FlatList, Image, RefreshControl } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 //
